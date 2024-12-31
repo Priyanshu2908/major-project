b02b6ab33ed01a9b0881b7e3e45dc707ba06bf50
Priyanshu2908 <prianshuchaubey29@gmail.com>
2024-12-31 13:48:57 +0530

    commit

---
+++ javascript/index.js
@@ -0,0 +1,36 @@
+console.log('Hello, World!');
+
+a = 10;
+console.log(a);
+
+msg = 'Hello, Good Morning!';
+
+console.log(msg);
+
+console.log(typeof a);
+console.log(typeof msg);
+
+ishungry = null;
+console.log(ishungry);
+
+ishungry = true;
+console.log(typeof ishungry);
+
+console.log(1 == 1);
+console.log(1 == 2);
+console.log(1 == "1");
+console.log(1 === "1");
+
+console.log(2 ** 4);
+
+console.log(10 % 3);
+console.log(10 / 3);
+
+console.log(1+1);
+console.log("1"+1);
+console.log('1'+"1");
+console.log('My name is ' +msg+ 'John Doe');
+
+
+
+
